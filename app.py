6cc3a2bf070db72ef71546eb41e41b6ba8a7e1b4
perikosinc <perikosinc@gmail.com>
2021-09-25 19:21:43 -0500

    Configuración de rutas

--- app.py
+++ app.py
@@ -19,5 +19,21 @@ app.secret_key = 'mysecretkey'
 def Index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(port = 7000, debug = True)
+@app.route('/register')
+def Register():
+    return render_template('./account/register.html')
+
+@app.route('/login')
+def Login():
+    return render_template('./account/login.html')
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
+    else:
+        return jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR']}), 200
+    # return jsonify(Origin=request.headers.get('X-Forwarded-For', request.remote_addr))
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=8000)
